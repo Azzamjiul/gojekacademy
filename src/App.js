0e0db73420d4ac95f889389096eeb7bb1c9ebb0e
Azzamjiul <34801766+Azzamjiul@users.noreply.github.com>
2020-09-05 15:14:15 +0700

    add search profile element

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
 import React, { useState } from "react";
 import "./styles.css";
 
+const Post = ({ src }) => <img src={src} alt="profile" />;
+
 const Profile = (props) => {
+  const { username } = props;
   const [isFollowed, setIsFollowed] = useState(false);
+  const [inputAccount, setInputAccount] = useState(false);
+  const postCount = 5;
+  const posts = [...Array(5)];
+
   return (
     <React.Fragment>
-      <img src="https://via.placeholder.com/150" alt="Profile Picture" />
-      <div>@{props.username}</div>
+      <input
+        placeholder="Search"
+        onChange={(e) => {
+          setInputAccount(e.target.value);
+        }}
+      />
+      <div>{inputAccount}</div>
+      <img src="https://via.placeholder.com/150" alt="Profile" />
+      <div>@{username}</div>
       <button onClick={() => setIsFollowed(!isFollowed)}>
         {isFollowed ? "Unfollow" : "Follow"}
       </button>
-      <div>Posts</div>
+      <div>{postCount} Posts</div>
       <div>Followers</div>
       <div>Following</div>
       <div>Bio</div>
+      {postCount === 0 ? (
+        <div>No Posts</div>
+      ) : (
+        posts.map((_, idx) => (
+          <Post src="https://via.placeholder.com/150" key={idx} />
+        ))
+      )}
     </React.Fragment>
   );
 };
